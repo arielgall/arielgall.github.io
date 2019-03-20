b9eac9c3a29056ec42eb0f4b06c9980b6087bd7d
Ariel Gall <arielgall16@gmail.com>
2019-03-19 18:42:02 -0700

    Styling Content

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,24 @@
 console.log('OK');
 
 // Light Gallery
-$('.gallery-1').lightGallery({
-    download: false,
-})
+
+// Controls for Views
+$('#grid').click( function(){
+  
+    // Remove List
+  $('main').removeClass('list');
+  
+  $('main').addClass('grid');
+  // console.log('Grid Click');
+
+});
+
+$('#list').click( function(){
+  
+  // Remove Grid
+  $('main').removeClass('grid');
+  
+  $('main').addClass('list');
+  // console.log('List Click');
+  
+});
